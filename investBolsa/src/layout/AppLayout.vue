d9cfc9dc210f2749ab00f9688c70dfe4d27c741e
JoaoVictorNascimento <joao_victorxy@hotmail.com>
2023-09-18 15:02:16 -0300

    feat: create initial pages dashboard, stocksPage and stocknews, create routes, remove unused components

---
+++ investBolsa/src/layout/AppLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <a-layout class="layout">
+        <a-layout-header>
+            <div class="logo" />
+        </a-layout-header>
+        <a-layout-content>
+            <div class="content-layout">
+                <router-view />
+            </div>
+        </a-layout-content>
+        <a-layout-footer>
+            Copyright © 2023 InvestBolsa. 
+        </a-layout-footer>
+    </a-layout>
+</template>
+
+<style scoped lang="scss">
+    .content-layout {
+        padding: 24px;
+        height: 100%;
+    }
+</style>
--- investBolsa/src/router/index.ts
+++ investBolsa/src/router/index.ts
@@ -1,22 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import AppLayout from '@/layout/AppLayout.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: AppLayout,
+      children: [
+        {
+          path: '/',
+          name: 'dashboard',
+          component: () => import('@/views/Dashboard.vue')
+        },
+        {
+          path: '/stocks',
+          name: 'stocks-page',
+          component: () => import('@/views/StocksPage.vue')
+        },
+        {
+          path: '/stock-news',
+          name: 'stock-news',
+          component: () => import('@/views/StockNews.vue')
+        },
+      ]
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
